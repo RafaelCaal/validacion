3de80743d136e07dbd81df5f5aa10f533854e230
Rafael Caal <rafael.caal@gmail.com>
2023-03-06 21:58:19 -0600

    second commit

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ let ventana;
 
 function createWindow(){
     ventana = new BrowserWindow({
-        width: 500,
-        height:300
+        width: 400,
+        height:500
     })
     ventana.loadFile('index.html')
 }
